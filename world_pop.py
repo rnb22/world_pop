10081c1d13be88f53f4fe33fba29d3d645598095
Rana N <108360455+rnb22@users.noreply.github.com>
2022-09-25 23:59:47 +0300

    Update world_pop.py

--- world_pop.py
+++ world_pop.py
@@ -1,7 +1,6 @@
 ### Importing Packages###
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt 
 import numpy as np
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
